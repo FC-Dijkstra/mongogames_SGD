bb01aa05e8e644e08f17304f0d3884ac604b5aa0
Aysao <matt.891999@hotmail.fr>
2022-04-17 17:15:58 +0200

    Ajout CRUD Order + ajout pprint buyer

--- src/scripts/getAllOrder.py
+++ src/scripts/getAllOrder.py
@@ -3,6 +3,7 @@ import json
 import pprint
 from datetime import datetime
 from bson.objectid import ObjectId
+from pprint import pprint
 import pymongo
 
 configfile = open("../../config.json")
@@ -19,12 +20,9 @@ client = pymongo.MongoClient(mongostring)
 db = client.SGD
 print("--- Connection OK ---")
 print("--- find all buyers ---")
-updated_buyer = db.buyers.find()
-for buyer in updated_buyer:
-        print("{")
-        for item in buyer:
-                print(item," : ", buyer[item])
-        print("}")
+orders = db.orders.find()
+for order in orders:
+        pprint(order)
 
 
 print("--- Closing connexion ---")
